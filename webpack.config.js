e937408020c4088a1086348cb9ae43487f122e3c
Stefano Sonzogni <stefano788@gmail.com>
2021-07-23 09:45:35 +0200

    setup relativeci

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var BrotliPlugin = require('brotli-webpack-plugin');
+var RelativeCiAgentWebpackPlugin = require('@relative-ci/agent');
+
 module.exports = {
-    plugins: [
-        new BrotliPlugin({
-            asset: '[path].br',
-            threshold: 0,
-            minRatio: 0.8,
-    })
-  ]
-}
+  plugins: [
+    new RelativeCiAgentWebpackPlugin(),
+    new BrotliPlugin({
+      asset: '[path].br',
+      threshold: 0,
+      minRatio: 0.8,
+    }),
+  ],
+};
