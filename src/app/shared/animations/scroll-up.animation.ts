9657cad4eb044ee7c7b2b4cb2b872e2b1ca7213d
Stefano Sonzogni <stefano788@gmail.com>
2024-02-20 17:33:47 +0100

    fix animations

--- src/app/shared/animations/scroll-up.animation.ts
+++ src/app/shared/animations/scroll-up.animation.ts
@@ -1,22 +1,22 @@
-import { animate, sequence, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, state, style, transition, trigger } from '@angular/animations';
 
 import { VisibilityState } from '@app/enums';
 
 export const scrollUpAnimation = trigger('scrollUpAnimation', [
-  state(VisibilityState.Hidden, style({ transform: 'translateY(-100%)', opacity: 0 })),
-  state(VisibilityState.Visible, style({ transform: 'translateY(0)', opacity: 1 })),
+  state(VisibilityState.Hidden, style({ transform: 'translateY(-100%)' })),
+  state(VisibilityState.Visible, style({ transform: 'translateY(0)' })),
   transition(
     `${VisibilityState.Hidden} => ${VisibilityState.Visible}`,
-    sequence([
-      animate(50, style({ opacity: 1 })),
-      animate('500ms cubic-bezier(0.05, 0.7, 0.1, 1.0)', style({ transform: 'translateY(0)' })),
+    group([
+      query('.mat-toolbar-row', animate(300, style({ opacity: 1 }))),
+      animate('400ms cubic-bezier(0.05, 0.7, 0.1, 1.0)', style({ transform: 'translateY(0)' })),
     ]),
   ),
   transition(
     `${VisibilityState.Visible} => ${VisibilityState.Hidden}`,
-    sequence([
+    group([
       animate('200ms cubic-bezier(0.3, 0.0, 0.8, 0.15)', style({ transform: 'translateY(-100%)' })),
-      animate(50, style({ opacity: 0 })),
+      query('.mat-toolbar-row', animate(150, style({ opacity: 0 }))),
     ]),
   ),
 ]);
