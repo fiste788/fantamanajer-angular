771eb807d75311ff4096e136beee2b396f445438
Stefano Sonzogni <stefano788@gmail.com>
2024-03-11 17:16:35 +0100

    window control override bis

--- src/app/core/services/native-navigator.service.ts
+++ src/app/core/services/native-navigator.service.ts
@@ -1,3 +1,9 @@
+declare global {
+  interface Navigator {
+    windowControlsOverlay?: WindowControlsOverlay;
+  }
+}
+
 export abstract class NavigatorRef {
   get nativeNavigator(): Navigator | object {
     throw new Error('Not implemented.');
