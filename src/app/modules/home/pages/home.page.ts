91acc4ccb14f547d3c6b2772e8a17fd7c8185de8
Stefano Sonzogni <stefano788@gmail.com>
2024-09-23 17:46:06 +0200

    fix wrong import

--- src/app/modules/home/pages/home.page.ts
+++ src/app/modules/home/pages/home.page.ts
@@ -13,7 +13,6 @@ import { Member, Role } from '@data/types';
 import { cardCreationAnimation } from '@shared/animations';
 import { MatEmptyStateComponent } from '@shared/components/mat-empty-state';
 import { PlayerImageComponent } from '@shared/components/player-image';
-import { ServerSidePreventAnimationReplayDirective } from '@shared/directives/server-side-prevent-replay-animation.directive';
 
 import { BestPlayersListComponent } from '../components/best-players-list/best-players-list.component';
 
