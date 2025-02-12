0e72824c74c9dbfa1585d51ae48eb6f7cf2220d1
Stefano Sonzogni <stefano788@gmail.com>
2025-02-12 16:25:11 +0100

    add deployurl and base href to fix relative path errors

--- esbuild/index-html-transformer.ts
+++ esbuild/index-html-transformer.ts
@@ -1,11 +1,7 @@
 import { minify } from 'html-minifier';
 
 const minifyHtml = (indexHtml: string): string => {
-  const fixed = indexHtml.replaceAll(
-    '<link rel="modulepreload" href="chunk-',
-    '<link rel="modulepreload" href="/chunk-',
-  );
-  const minified = minify(fixed, {
+  const minified = minify(indexHtml, {
     collapseWhitespace: true,
     minifyJS: true,
     removeAttributeQuotes: true,
