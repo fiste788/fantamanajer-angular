71acb699cf087ec9e1a185b43b0bab388ec364be
Stefano Sonzogni <stefano788@gmail.com>
2025-11-21 14:00:57 +0100

    fix: championship resolver part two

--- src/app/modules/championship/pages/championship/championship.resolver.ts
+++ src/app/modules/championship/pages/championship/championship.resolver.ts
@@ -3,14 +3,16 @@ import { toObservable } from '@angular/core/rxjs-interop';
 import { ResolveFn } from '@angular/router';
 import { map, take } from 'rxjs';
 
+import { filterNil } from '@app/functions';
 import { ApplicationService } from '@app/services';
 import { Championship } from '@data/types';
 
 export const championshipResolver: ResolveFn<Championship | undefined> = () => {
   const applicationService = inject(ApplicationService);
-  const teamSignal = applicationService.requireCurrentTeam;
+  const teamSignal = applicationService.currentTeam;
 
   return toObservable(teamSignal).pipe(
+    filterNil(),
     map((team) => team.championship),
     take(1),
   );
