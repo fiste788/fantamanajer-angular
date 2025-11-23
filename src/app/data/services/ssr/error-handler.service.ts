ccf37b727dd8ffc2d384e055df8da9cfc90cac8e
Stefano Sonzogni <stefano788@gmail.com>
2025-11-23 22:29:03 +0100

    fix request context

--- src/app/data/services/ssr/error-handler.service.ts
+++ src/app/data/services/ssr/error-handler.service.ts
@@ -1,6 +1,4 @@
-import { ErrorHandler, inject, Injectable, InjectionToken, Provider } from '@angular/core';
-
-export const SSR_STATUS_TOKEN = new InjectionToken<SSRStatus>('SSR_STATUS');
+import { ErrorHandler, inject, Injectable, Provider, REQUEST_CONTEXT } from '@angular/core';
 
 /**
  * TIPO ESTERNO PER LO STATO DI FALLIMENTO SSR (Rinominato e semplificato)
@@ -11,13 +9,19 @@ export interface SSRStatus {
   error?: unknown;
 }
 
+export interface RequestContext {
+  ctx: ExecutionContext;
+  nonce?: string;
+  ssrStatus: SSRStatus;
+}
+
 @Injectable()
 export class ServerSideErrorHandler extends ErrorHandler {
-  readonly #ssrStatus = inject<SSRStatus>(SSR_STATUS_TOKEN, { optional: true });
+  readonly #requestContext = inject<RequestContext>(REQUEST_CONTEXT, { optional: true });
 
   public override handleError(error: unknown): void {
-    if (this.#ssrStatus) {
-      this.#ssrStatus.error = error; // <--- AGGIUNTO IL SALVATAGGIO DELL'ERRORE
+    if (this.#requestContext) {
+      this.#requestContext.ssrStatus.error = error; // <--- AGGIUNTO IL SALVATAGGIO DELL'ERRORE
     } else {
       console.error('Server error handler', error);
     }
